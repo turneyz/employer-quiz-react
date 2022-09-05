2df1decda76b1fbd0f427f5df988a53120e9614a
Zachary Turney <99896219+turneyz@users.noreply.github.com>
2022-09-04 21:36:41 -0700

    Added in base code for questions and answers and rendered base DOM template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useState} from 'react'
+ 
 function App() {
+
+  const questions = [
+    {
+      questionText: 'Sample Question 1',
+      answerOptions: [
+        {answerText: 'Answer 1', isCorrect: false},
+        {answerText: 'Answer 2', isCorrect: false},
+        {answerText: 'Answer 3', isCorrect: true},
+        {answerText: 'Answer 4', isCorrect: false}
+      ]
+    },
+    {
+      questionText: 'Sample Question 2',
+      answerOptions: [
+        {answerText: 'Answer 1', isCorrect: false},
+        {answerText: 'Answer 3', isCorrect: true},
+        {answerText: 'Answer 2', isCorrect: false},
+        {answerText: 'Answer 4', isCorrect: false}
+      ]
+    },
+    {
+      questionText: 'Sample Question 3',
+      answerOptions: [
+        {answerText: 'Answer 3', isCorrect: true},
+        {answerText: 'Answer 1', isCorrect: false},
+        {answerText: 'Answer 2', isCorrect: false},
+        {answerText: 'Answer 4', isCorrect: false}
+      ]
+    },
+    {
+      questionText: 'Sample Question 4',
+      answerOptions: [
+        {answerText: 'Answer 1', isCorrect: false},
+        {answerText: 'Answer 2', isCorrect: false},
+        {answerText: 'Answer 4', isCorrect: false},
+        {answerText: 'Answer 3', isCorrect: true}
+      ]
+    },
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {false ? (
+        <div className='outcomeBox'>You scored...</div>
+      ) : (
+        <>
+          <div className='quizBox'></div>
+            <div className='questionCount'>
+              <span>Question 1</span>/{questions.length}
+            </div>
+            <div className='questionText'>This is the question text</div>
+            <div className='answerSection'>
+              <button>Answer 1</button>
+              <button>Answer 2</button>
+              <button>Answer 3</button>
+              <button>Answer 4</button>
+            </div>
+        </>
+      )}
     </div>
   );
 }

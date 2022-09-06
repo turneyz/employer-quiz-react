5a36531d3ec2b3a681a99dc06d7a19d4ad89e8a0
Zachary Turney <99896219+turneyz@users.noreply.github.com>
2022-09-05 18:52:01 -0700

    Additional work on quiz layout rendering, base template complete

--- src/App.js
+++ src/App.js
@@ -6,29 +6,29 @@ function App() {
 
   const questions = [
     {
-      questionText: 'Sample Question 1',
+      questionText: 'Is your company in search of a(n)',
       answerOptions: [
-        {answerText: 'Answer 1', isCorrect: false},
-        {answerText: 'Answer 2', isCorrect: false},
-        {answerText: 'Answer 3', isCorrect: true},
-        {answerText: 'Answer 4', isCorrect: false}
+        {answerText: 'Senior Web Developer', isCorrect: false},
+        {answerText: 'Carnival Clown', isCorrect: false},
+        {answerText: 'Entry Level Engineer', isCorrect: true},
+        {answerText: 'Cloud Architect', isCorrect: false}
       ]
     },
     {
       questionText: 'Sample Question 2',
       answerOptions: [
         {answerText: 'Answer 1', isCorrect: false},
-        {answerText: 'Answer 3', isCorrect: true},
-        {answerText: 'Answer 2', isCorrect: false},
+        {answerText: 'Answer 2', isCorrect: true},
+        {answerText: 'Answer 3', isCorrect: false},
         {answerText: 'Answer 4', isCorrect: false}
       ]
     },
     {
       questionText: 'Sample Question 3',
       answerOptions: [
-        {answerText: 'Answer 3', isCorrect: true},
-        {answerText: 'Answer 1', isCorrect: false},
+        {answerText: 'Answer 1', isCorrect: true},
         {answerText: 'Answer 2', isCorrect: false},
+        {answerText: 'Answer 3', isCorrect: false},
         {answerText: 'Answer 4', isCorrect: false}
       ]
     },
@@ -37,28 +37,53 @@ function App() {
       answerOptions: [
         {answerText: 'Answer 1', isCorrect: false},
         {answerText: 'Answer 2', isCorrect: false},
-        {answerText: 'Answer 4', isCorrect: false},
-        {answerText: 'Answer 3', isCorrect: true}
+        {answerText: 'Answer 3', isCorrect: false},
+        {answerText: 'Answer 4', isCorrect: true}
       ]
     },
   ]
 
+  const [currQuestion, setCurrQuestion] = useState(0)
+
+  const [showScore, setShowScore] = useState(false)
+
+  const [score, setScore] = useState(0)
+
+  const handleAnswerButtonClick = (isCorrect) => {
+    if (isCorrect === true) {
+      setScore(score + 1)
+    }
+
+    const nextQuestion = currQuestion + 1
+    if (nextQuestion < questions.length) {
+      setCurrQuestion(nextQuestion)
+    } else {
+      setShowScore(true)
+    }
+  }
+
   return (
     <div className="App">
-      {false ? (
-        <div className='outcomeBox'>You scored...</div>
+      {showScore ? (
+        <div className='outcomeBox'>You scored {score} out of {questions.length}</div>
       ) : (
         <>
-          <div className='quizBox'></div>
-            <div className='questionCount'>
-              <span>Question 1</span>/{questions.length}
+          <div className='container'>
+            <div className='info'>
+              <div className='questionNumber'>
+                <span>Question {currQuestion + 1}</span>/{questions.length}
+              </div>
+              
+              <div className='questionText'>{questions[currQuestion].questionText}</div>
             </div>
-            <div className='questionText'>This is the question text</div>
+            
             <div className='answerSection'>
-              <button>Answer 1</button>
-              <button>Answer 2</button>
-              <button>Answer 3</button>
-              <button>Answer 4</button>
+              {questions[currQuestion].answerOptions.map((answerOption) => (
+                <button onClick={() => handleAnswerButtonClick(answerOption.isCorrect)}>
+                  {answerOption.answerText}
+                </button>
+              ))}
+            </div>
             </div>
         </>
       )}
